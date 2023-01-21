6d6d20de5f0f4e95f8ca5b9d0b544bcdc9cf0d6e
amankr1635 <amankr.1635@gmail.com>
2023-01-21 20:15:43 +0530

    final code

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const route = require('./routes/route');
 
 app.use(express.json());
 app.use(multer().any())
-mongoose.connect("mongodb+srv://Amankr:pwwELCe59UIUh9mj@cluster0.oxwexg5.mongodb.net/project-urlShortner")
+mongoose.connect("mongodb+srv://Amankr:pwwELCe59UIUh9mj@cluster0.oxwexg5.mongodb.net/group13Database")
 .then(()=>console.log("Mongo DB is connected"))
 .catch(err=> console.log(err))
 
